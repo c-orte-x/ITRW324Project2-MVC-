a4e8fd6bb7c42d7cdd3e8dcfdb8761c53cb729d5
c-orte-x <36413036+c-orte-x@users.noreply.github.com>
2019-09-10 00:58:49 +0200

    Added view where users can view bookings without making them. Users can no longer make bookings without being logged in

--- ITRW324Project2(MVC)/Controllers/HomeController.cs
+++ ITRW324Project2(MVC)/Controllers/HomeController.cs
@@ -31,6 +31,12 @@ namespace Final.Controllers
             return View(result);
         }
 
+        public IActionResult ViewBooking() //ReturnViewBooking View
+        {
+            var result = _context.Booking.ToList();
+            return View(result);
+        }
+
         public IActionResult Rates() //Retrun Rates View
         {
             return View();
---
+++ ITRW324Project2(MVC)/Views/Home/ViewBooking.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Final.Views.Home
+{
+    public class ViewBookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
